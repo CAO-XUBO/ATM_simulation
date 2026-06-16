ddc904a9a901756c71c6de8b6fed31a429badb1e
caoxubo <caoxb2004@163.com>
2026-06-16 15:09:55 +0100

    Code framwork

--- Config.py
+++ Config.py
@@ -3,3 +3,10 @@
 # Energy consumption
 P_BUSY = 240
 P_IDLE = 150
+
+# Experimetal Settings
+ARRIVAL_RATE = 1.5 # lambda
+SERVICE_RATE = 1.0 # mu
+SIMULATION_TIME = 100
+NUM_SERVERS = 5
+SEED = 42
---
+++ main.py
@@ -0,0 +1,4 @@
+from simulator import *
+
+results = multi_ATM_simulator()
+print(results)
