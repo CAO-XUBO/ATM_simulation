880de549e2c26af4b2808c2f236d0e5d17caec5e
caoxubo <caoxb2004@163.com>
2026-06-22 00:27:53 +0100

    adjust

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from simulator import *
 from Config import *
 
-results = server_simulator(Num_server=NUM_SERVERS, arrival_rate=ARRIVAL_RATE, service_rate=SERVICE_RATE,
-                           timesteps=SIMULATION_TIME, seed=SEED)
+Average_System_Size, Utilization, Average_Power, Average_Waiting_Time, Average_Response_Time, ERP  = server_simulator(
+        Num_server = 5,
+        arrival_rate = ARRIVAL_RATE,
+        service_rate = SERVICE_RATE,
+        timesteps = SIMULATION_TIME,
+        policy = "INSTANTOFF",  # "INSTANTOFF", "NEVEROFF"
+        seed = 42)
 
-print(results)
+print("Simulation Finished")
+print("The Average System Size:", Average_System_Size)
+print("Utilization:", Utilization)
+print("Average Power:", Average_Power)
+print("Average Waiting Time:", Average_Waiting_Time)
+print("Average Response Time:", Average_Response_Time)
+print("ERP:", ERP)
