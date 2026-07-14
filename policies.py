0e19405538b73ae2804744c80fe4979235d18aa3
caoxubo <caoxb2004@163.com>
2026-07-14 15:25:19 +0100

    some adjust about the instantoff policy turn-on rules. And the threshold policy turn-on function

--- policies.py
+++ policies.py
@@ -21,23 +21,53 @@ def should_start_setup_instantoff(central_queue, server_state):
     '''
     queue_length = len(central_queue)
 
-    idle_servers = count_state(server_state, "IDLE")
-    setup_servers = count_state(server_state, "SETUP")
-    off_servers = count_state(server_state, "OFF")
+    Num_idle_servers = count_state(server_state, "IDLE")
+    Num_setup_servers = count_state(server_state, "SETUP")
+    Num_off_servers = count_state(server_state, "OFF")
 
     if queue_length == 0:
         return False
 
-    if idle_servers > 0:
+    if Num_idle_servers > 0:
         return False
 
-    if off_servers == 0:
+    if Num_off_servers == 0:
         return False
 
-    if setup_servers >= queue_length:
+    if Num_setup_servers >= queue_length:
         return False
 
     return True
+
+
+def should_start_setup_threshold(central_queue, server_state, turn_on_threshold):
+    '''
+    The turn-on rule for Threshold policy
+    turn_on_threshold: T_o
+    '''
+
+    Num_idle_servers = count_state(server_state, "IDLE")
+    Num_off_servers = count_state(server_state, "OFF")
+
+    if Num_off_servers == 0:
+        return False
+
+    # Case I: when T_o is positive, the policy focus on the number of idle servers
+    if turn_on_threshold > 0:
+        if Num_idle_servers < turn_on_threshold:
+            return True
+        else:
+            return False
+
+    # Case II: when T_o is negative, the policy focus on the length of central queue
+    else:
+        queue_length = len(central_queue)
+        queue_threshold = abs(turn_on_threshold)
+
+        if queue_length >= queue_threshold:
+            return True
+        else:
+            return False
 
 def get_policy_functions(policy):
     if policy == "NEVEROFF":
